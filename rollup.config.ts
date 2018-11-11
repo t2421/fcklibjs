c2420cde1b677615f1f0eb069e21210cce7f61f6
t2421 <t2421gm@gmail.com>
2018-11-11 12:09:15 +0900

    fcklibjs用に出力ファイル調整

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'lib-typescript-starter'
+const libraryName = 'fcklibjs'
 
 export default {
   input: `src/${libraryName}.ts`,
