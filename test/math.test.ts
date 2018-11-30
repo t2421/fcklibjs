bc6c4472baa6f6526a581d3d97d0bb4b8f388804
t2421 <t2421gm@gmail.com>
2018-11-30 20:58:23 +0900

    テスト追加

--- test/math.test.ts
+++ test/math.test.ts
@@ -3,4 +3,10 @@ import MathUtil from '../src/math/MathUtil'
 test('MathUtil test', () => {
   expect(MathUtil.map(100, 0, 100, 0, 200)).toBe(200)
   expect(MathUtil.map(50, 0, 100, 0, 200)).toBe(100)
+
+  expect(MathUtil.toDegree(Math.PI)).toBe(180)
+  expect(MathUtil.toDegree(2 * Math.PI)).toBe(360)
+
+  expect(MathUtil.toRadian(360)).toBe(2 * Math.PI)
+  expect(MathUtil.toRadian(180)).toBe(Math.PI)
 })
