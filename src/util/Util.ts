23c5b98332e726017ab893e8feae3476da880ec5
t2421 <t2421gm@gmail.com>
2018-11-30 19:19:31 +0900

    Util.getUrlParam実装

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -1,8 +1,14 @@
 import 'url-polyfill'
 
 export default class Util {
-  public static getUrlParam(url: string) {
-    const parse = new URL(url)
-    console.log(parse)
+  public static getUrlParam(url: string): Object {
+    const parseUrl: URL = new URL(url)
+    var arg = new Object()
+    var pair = parseUrl.search.substring(1).split('&')
+    for (var i = 0; pair[i]; i++) {
+      var kv = pair[i].split('=')
+      arg[kv[0]] = kv[1]
+    }
+    return arg
   }
 }
--- test/util.test.ts
+++ test/util.test.ts
@@ -1,5 +1,7 @@
 import Util from '../src/util/Util'
 
-test('Util test', done => {
-  Util.getUrlParam('https://yahoo.co.jp?hogehoge=10&hogehoge2=30#anchor')
+test('Util test', () => {
+  const url: Object = Util.getUrlParam('https://yahoo.co.jp?hogehoge=10&hogehoge2=30#anchor')
+  expect(url['hogehoge']).toBe('10')
+  expect(url['hogehoge2']).toBe('30')
 })
