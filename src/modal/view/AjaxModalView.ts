18b6e85d51e30e968d9b4017bc0324106db602c4
t2421 <t2421gm@gmail.com>
2018-11-30 18:57:08 +0900

    rollupからwebpackへ変更

--- src/modal/view/AjaxModalView.ts
+++ src/modal/view/AjaxModalView.ts
@@ -1,34 +1,30 @@
-import ModalView from './ModalView';
-import axios from 'axios';
+import ModalView from './ModalView'
+import axios from 'axios'
+const $ = require('jquery')
 
-const $ = require('jquery');
-export default class AjaxModalView implements ModalView{
-    private config:any;
-    constructor(config:any){
-        this.config = config;
-    }
+export default class AjaxModalView implements ModalView {
+  private config: any
+  constructor(config: any) {
+    this.config = config
+  }
 
-    getView():Promise<string>{
-        
-        return new Promise(
-        (resolve: (value?:string) => void, reject: () => void) => {
-            
-            axios
-            .get(this.config.src)
-            .then((res:any) => {
-               console.log(res.data);
-                const html = this.getHtml(res.data);
-                resolve(html);
-            })
-            .catch((error:any) => {
-                
-                resolve("error");
-            })
-            console.log("call")
-        });
-    }
+  getView(): Promise<string> {
+    return new Promise((resolve: (value?: string) => void, reject: () => void) => {
+      axios
+        .get(this.config.src)
+        .then((res: any) => {
+          console.log(res.data)
+          const html = this.getHtml(res.data)
+          resolve(html)
+        })
+        .catch((error: any) => {
+          resolve('error')
+        })
+      console.log('call')
+    })
+  }
 
-    private getHtml(data:any):string{
-        return `${data.name}`;
-    }
+  private getHtml(data: any): string {
+    return `${data.name}`
+  }
 }
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    mode: 'development',
+
+    entry: './src/fcklibjs.ts',
+    output:{
+        path:`${__dirname}/dist`,
+        filename:'main.js',
+        //umd形式での出力
+        library: "fcklib",
+        libraryTarget: "umd"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader'
+        }
+      ]
+    },
+    resolve: {
+      extensions: [
+        '.ts','.js'
+      ]
+    }
+  };
+  
